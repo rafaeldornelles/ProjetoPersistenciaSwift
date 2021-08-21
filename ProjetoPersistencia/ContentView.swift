1969c23a137843de68c8aeb86d1d7aedc68c9f60
rafael.dornelles <rafafd@hotmail.com>
2021-08-21 16:23:48 -0300

    Initial Commit

--- ProjetoPersistencia/ContentView.swift
+++ ProjetoPersistencia/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(entity: Pessoa.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Pessoa.nome, ascending: true)])
+    var pessoas: FetchedResults<Pessoa>
+    
+    
+    @State var nome = ""
+    @State var dataNascimento = Date()
+    @State var email = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView{
+            VStack{
+                TextField("Nome", text: $nome).textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                DatePicker(selection: $dataNascimento, label: { Text("Data Nascimento") })
+                Button("Salvar"){
+                    let pessoa = Pessoa(context: viewContext)
+                    pessoa.nome = nome
+                    pessoa.email = email
+                    pessoa.dataNascimento = dataNascimento
+                    PersistenceController.banco.save()
+                }
+            }.padding().tabItem { Text("Adicionar") }
+            
+            List{
+                ForEach(pessoas){ item in
+                    VStack{
+                        Text("\(item.nome ?? "")")
+                        if let dtNasc = item.dataNascimento{
+                            Text("\(formatDate(date:  dtNasc))")
+                        }
+                        
+                        Text("\(item.email ?? "")")
+                    }
+                }.onDelete(perform: deleteItems)
+                
+            }.padding().tabItem { Text("Lista") }
+            
+        }
+    }
+    
+    private func deleteItems(at offset: IndexSet) {
+        for index in offset{
+            let pessoa = pessoas[index]
+            PersistenceController.banco.delete(pessoa)
+        }
     }
 }
 
@@ -18,4 +64,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func formatDate(date: Date) -> String{
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "dd/MM/yyyy"
+    dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+    return dateFormatter.string(from: date)
 }
--- ProjetoPersistencia/ProjetoPersistenciaApp.swift
+++ ProjetoPersistencia/ProjetoPersistenciaApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct ProjetoPersistenciaApp: App {
+    let persistanceController = PersistenceController.banco
+    @Environment(\.scenePhase) var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+                .environment(\.managedObjectContext, persistanceController.container.viewContext)
+        }.onChange(of: scenePhase){  (newScenePhase) in
+            switch newScenePhase{
+                case .background:
+                    persistanceController.save()
+                    print("Está em background")
+                case .inactive:
+                    print("Esta inativo")
+                case .active:
+                    print("Está ativo")
+                @unknown default:
+                    print("Default")
+            }
+            
+    }
     }
 }
