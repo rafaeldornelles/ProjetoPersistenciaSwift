0434703cb4cf6f7cd3b3272ea6860ccff58b815b
rafael.dornelles <rafafd@hotmail.com>
2021-08-21 15:47:07 -0300

    Initial Commit

---
+++ ProjetoPersistencia/ProjetoPersistenciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoPersistenciaApp.swift
+//  ProjetoPersistencia
+//
+//  Created by IOS SENAC on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoPersistenciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
